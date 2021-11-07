284f7da65f388e197cb0334f30f96a8f42e8c466
msisaif <msi132043@gmail.com>
2021-11-08 00:32:26 +0600

    Date Filter Trait

--- app/Traits/DateFilter.php
+++ app/Traits/DateFilter.php
@@ -4,14 +4,14 @@ namespace App\Traits;
 
 trait DateFilter
 {
-    public function dateFilter()
+    public function dateFilter($column = 'created_at')
     {
         $this->getQuery()
-            ->when(request()->from, function ($query, $date_from) {
-                $query->whereDate('created_at', '>=', $date_from);
+            ->when(request()->from, function ($query, $date_from) use ($column) {
+                $query->whereDate($column, '>=', $date_from);
             })
-            ->when(request()->to, function ($query, $date_to) {
-                $query->whereDate('created_at', '<=', $date_to);
+            ->when(request()->to, function ($query, $date_to) use ($column) {
+                $query->whereDate($column, '<=', $date_to);
             });
 
         return $this;
